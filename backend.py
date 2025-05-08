724767b24941083ee6703ade52e88cd3cf605100
Brot <sepp2017.huber@gmail.com>
2025-05-08 10:45:42 +0200

    Add initial project files and backend logic

---
+++ backend.py
@@ -0,0 +1,12 @@
+"""
+Backend for the FSST Gituebung project.
+This module contains the backend logic for the project, handling the number generation and comparing
+"""
+#import ui
+import random
+
+number = random.randint(1, 100)
+
+print ("Welcome to the number guessing game!, I have selected a number between 1 and 100.")
+print ("But you can't guess it yet")
+
